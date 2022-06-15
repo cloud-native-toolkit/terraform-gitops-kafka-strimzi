761559c189e1919e8e28e3f0905bc2ce9a64a24e
tcskill <tskill@us.ibm.com>
2022-06-15 13:50:08 -0500

    init push

--- outputs.tf
+++ outputs.tf
@@ -2,35 +2,53 @@
 output "name" {
   description = "The name of the module"
   value       = local.name
-  depends_on  = [null_resource.setup_gitops]
+  depends_on  = [gitops_module.kafka_app]
 }
 
 output "branch" {
   description = "The branch where the module config has been placed"
   value       = local.application_branch
-  depends_on  = [null_resource.setup_gitops]
+  depends_on  = [gitops_module.kafka_app]
 }
 
 output "namespace" {
   description = "The namespace where the module will be deployed"
   value       = local.namespace
-  depends_on  = [null_resource.setup_gitops]
+  depends_on  = [gitops_module.kafka_app]
 }
 
 output "server_name" {
   description = "The server where the module will be deployed"
   value       = var.server_name
-  depends_on  = [null_resource.setup_gitops]
+  depends_on  = [gitops_module.kafka_app]
 }
 
 output "layer" {
   description = "The layer where the module is deployed"
   value       = local.layer
-  depends_on  = [null_resource.setup_gitops]
+  depends_on  = [gitops_module.kafka_app]
 }
 
 output "type" {
   description = "The type of module where the module is deployed"
   value       = local.type
-  depends_on  = [null_resource.setup_gitops]
+  depends_on  = [gitops_module.kafka_app]
+}
+
+output "user_name" {
+  description = "The type of module where the module is deployed"
+  value       = local.type
+  depends_on  = [gitops_module.kafka_app]
+}
+
+output "user_password" {
+  description = "The type of module where the module is deployed"
+  value       = local.type
+  depends_on  = [gitops_module.kafka_app]
+}
+
+output "clusterid" {
+  description = "The id of the kafka cluster name"
+  value       = var.cluster_name
+  depends_on  = [gitops_module.kafka_app]
 }
